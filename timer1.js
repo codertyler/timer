906bf0a74a035838943943f31ccede170d382068
codertyler <codertylerasai@gmail.com>
2020-09-22 20:56:23 +0000

    handled edges

--- timer1.js
+++ timer1.js
@@ -1,9 +1,29 @@
 let timeInput = process.argv.slice(2);
 
+let stringOrNo = "";
 
+for (const items of timeInput) {
+  if(typeof items === "string") {
+    stringOrNo = true;
+  } else {
+    stringOrNo = false;
+  }
+ }
 
-for (const time of timeInput) {
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, time * 1000);
+if (timeInput.length === 0) {
+  console.log("There was no timer input");
+} else if (timeInput < 0) {
+  console.log("You have entered a negative number, please provide a positive number")
+} else if (stringOrNo)
+ {
+  console.log("You didn't enter a number") 
+} else {
+  for (const time of timeInput) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, time * 1000);
+  }
 }
+
+
+
